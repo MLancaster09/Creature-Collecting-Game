e10778f077890c0615d98103608e7e02c89d0fd8
MLancaster09 <255350@esher.ac.uk>
2026-05-01 11:20:25 +0100

    fixed some bugs

--- nea.py
+++ nea.py
@@ -16,23 +16,32 @@ class Creature():
         self.defence = defence
         self.speed = speed
         self.CATCH_RATE = CATCH_RATE
-class Player():
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, name, creatures):
+        pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.x = x
-        self.image = pygame.image.load("player.png")
+        self.y = y
+        self.image = pygame.image.load("girlFront.png")
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
         self.journal = {}
         for c in creatures:
             self.journal[c]=False
     def updateJournal(self, creature):
         self.journal[creature]=True
+    def getSurface(self):
+        return self.image
+    def getRect(self):
+        return self.rect
     def move(self, x, y):
         pass
     
 class Map():
     pass
 
-class main():
+def main():
     def initialiseCreatures():
         with open("creatures.json", "r") as file:
             data = json.load(file) #load the data stored in the json file as a dictionary of dictionaries
@@ -44,29 +53,29 @@ class main():
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
     running = True
-    player = Player("player")
     creatures = initialiseCreatures()
+    player = Player(0, 0,"player", creatures)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.Key == pygame.K_s:
+                if event.key == pygame.K_s:
                     pass
-                elif event.Key == pygame.K_w:
+                elif event.key == pygame.K_w:
                     pass
-                elif event.Key == pygame.K_a:
+                elif event.key == pygame.K_a:
                     pass
-                elif event.Key == pygame.K_d:
+                elif event.key == pygame.K_d:
                     pass
             elif event.type == pygame.KEYUP:
                 pass
-        
-
         screen.fill("purple")
+        screen.blit(player.getSurface(), player.getRect())
         pygame.display.flip()
         clock.tick(60) 
-
+if __name__ == "__main__":
+    main()
 pygame.quit()
 
 
