1887bd538d67fb3a51b7032b52e37ed39d0dc5bc
MLancaster09 <255350@esher.ac.uk>
2026-05-01 14:19:12 +0100

    movement implemented

--- nea.py
+++ nea.py
@@ -8,30 +8,33 @@ class Item():
         self.name = n
         self.description = d
         self.quantity = q
-
+SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1280
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
     creatures = Creatures.initialiseCreatures()
-    player = Player.Player(0, 0,"player", creatures)
+    player = Player.Player(0, 0,"player", creatures, SCREEN_WIDTH, SCREEN_HEIGHT)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
-                    pass
+                    player.move(0,1)
                 elif event.key == pygame.K_w:
-                    pass
+                    player.move(0,-1)
                 elif event.key == pygame.K_a:
-                    pass
+                    player.move(-1,0)
                 elif event.key == pygame.K_d:
-                    pass
+                    player.move(1,0)
             elif event.type == pygame.KEYUP:
-                pass
+                if event.key in [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]:
+                    player.move(0,0)
         screen.fill("purple")
+        player.update()
         screen.blit(player.getSurface(), player.getRect())
         pygame.display.flip()
         clock.tick(60) 
--- player.py
+++ player.py
@@ -1,16 +1,21 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, name, creatures):
+    def __init__(self, x, y, name, creatures, SCREEN_WIDTH, SCREEN_HEIGHT):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.x = x
         self.y = y
+        self.changeX = 0
+        self.changeY = 0
         self.image = pygame.image.load("girlFront.png")
         self.image = pygame.transform.scale(self.image, (50, 80))
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
         self.journal = {}
         for c in creatures:
             self.journal[c]=False
@@ -20,6 +25,18 @@ class Player(pygame.sprite.Sprite):
         return self.image
     def getRect(self):
         return self.rect
+    def setXY(self, x, y):
+        self.x = x
+        self.y=y
+        self.rect.x = x
+        self.rect.y = y
     def move(self, x, y):
-        pass
-    
+        moveSize = 5
+        self.changeX = x*moveSize
+        self.changeY = y*moveSize
+    def update(self):
+        if self.changeX+self.x>self.SCREEN_WIDTH-(self.rect.width) or self.changeX+self.x<0:
+           self.changeX = 0
+        if self.changeY+self.y<0 or self.changeY+self.y>self.SCREEN_HEIGHT-(self.rect.height):
+           self.changeY = 0
+        self.setXY(self.x+self.changeX, self.y+self.changeY)
