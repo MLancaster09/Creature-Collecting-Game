8ef91dfe7242056ff9194a97823419da8dc01725
MLancaster09 <255350@esher.ac.uk>
2026-05-01 13:44:01 +0100

    committing for safety

--- nea.py
+++ nea.py
@@ -1,6 +1,6 @@
 import pygame
 import tiles
-import player
+import Player
 import Creatures
 
 class Item():
@@ -8,39 +8,14 @@ class Item():
         self.name = n
         self.description = d
         self.quantity = q
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, name, creatures):
-        pygame.sprite.Sprite.__init__(self)
-        self.name = name
-        self.x = x
-        self.y = y
-        self.image = pygame.image.load("girlFront.png")
-        self.rect=self.image.get_rect()
-        self.rect.x=x
-        self.rect.y=y
-        self.journal = {}
-        for c in creatures:
-            self.journal[c]=False
-    def updateJournal(self, creature):
-        self.journal[creature]=True
-    def getSurface(self):
-        return self.image
-    def getRect(self):
-        return self.rect
-    def move(self, x, y):
-        pass
-    
-class Map():
-    pass
 
 def main():
-
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
     running = True
     creatures = Creatures.initialiseCreatures()
-    player = Player(0, 0,"player", creatures)
+    player = Player.Player(0, 0,"player", creatures)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -1,1 +1,25 @@
 import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, name, creatures):
+        pygame.sprite.Sprite.__init__(self)
+        self.name = name
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("girlFront.png")
+        self.image = pygame.transform.scale(self.image, (50, 80))
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+        self.journal = {}
+        for c in creatures:
+            self.journal[c]=False
+    def updateJournal(self, creature):
+        self.journal[creature]=True
+    def getSurface(self):
+        return self.image
+    def getRect(self):
+        return self.rect
+    def move(self, x, y):
+        pass
+    
